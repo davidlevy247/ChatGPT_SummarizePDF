f710f30a23f740f70fd3c6befa9107f1e4961883
David Levy <3209302+davidlevy247@users.noreply.github.com>
2023-05-18 10:06:16 -0700

    Update pdf_summarizer.py
    added a preliminary encryption option.

--- pdf_summarizer.py
+++ pdf_summarizer.py
@@ -4,9 +4,68 @@ from PyPDF2 import PdfReader
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import re
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import base64
+import getpass
+
+CONFIG_FILE = 'config.txt'
+
+def get_encryption_key():
+    password = getpass.getpass("Enter your password: ").encode()  # Get the password for encryption
+    salt = b'\x00'*16  # Just for simplicity we use static salt
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+    )
+    return base64.urlsafe_b64encode(kdf.derive(password))
+
+def encrypt_data(data, key):
+    f = Fernet(key)
+    return f.encrypt(data)
+
+def decrypt_data(data, key):
+    f = Fernet(key)
+    return f.decrypt(data)
+
+def load_config(config_file, encryption_key=None):
+    if os.path.exists(config_file):
+        with open(config_file, 'rb') as f:
+            data = f.read()
+        if encryption_key is not None:
+            data = decrypt_data(data, encryption_key)
+        return data.decode().split('\n')
+
+    return None, None
+
+def save_config(config_file, api_key, prompt, encryption_key=None):
+    data = f"{api_key}\n{prompt}"
+    if encryption_key is not None:
+        data = encrypt_data(data.encode(), encryption_key)
+    else:
+        data = data.encode()
+    with open(config_file, 'wb') as f:
+        f.write(data)
+
+# Ask if user wants to use encryption
+use_encryption = input("Would you like to use encryption for the config file? [y/N]: ").strip().lower() in ['y', 'yes']
+
+# If config file exists, decrypt and read the content
+encryption_key = get_encryption_key() if use_encryption else None
+api_key, prompt = load_config(CONFIG_FILE, encryption_key)
+
+# If config file does not exist or user wants to change the config
+if api_key is None or prompt is None or \
+        input("Would you like to change the config? [y/N]: ").strip().lower() in ['y', 'yes']:
+    api_key = input("Enter your OpenAI API key: ")
+    prompt = input("Enter your prompt: ")
+    save_config(CONFIG_FILE, api_key, prompt, encryption_key)
 
 # Set up OpenAI API key
-openai.api_key = "your api key here"
+openai.api_key = api_key
 
 def summarize_text(text):
     # Use OpenAI API to summarize text
