e814dff078f11a9f52e84d7477f50e0abfcaa4be
David Levy <3209302+davidlevy247@users.noreply.github.com>
2023-05-18 10:47:56 -0700

    Update RunME.bat
    use text file for requirements

--- RunME.bat
+++ RunME.bat
@@ -5,8 +5,8 @@ REM Check if the virtual environment folder exists, if not create it and install
 if not exist "SummarizePDF" (
     python -m venv SummarizePDF
     call .\SummarizePDF\Scripts\activate.bat
-    python -m pip install --upgrade pip
-    python -m pip install PyPDF2 openai cryptography
+    call python -m pip install --upgrade pip
+    call python -m pip install -r requirements.txt
     call .\SummarizePDF\Scripts\deactivate.bat
 )
 
