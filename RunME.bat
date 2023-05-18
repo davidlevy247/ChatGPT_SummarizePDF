ea9820fbd4f2bae397c6bc01a46b0f17999c4b35
David Levy <3209302+davidlevy247@users.noreply.github.com>
2023-05-18 10:57:13 -0700

    Update RunME.bat
    fixed a logic issue

--- RunME.bat
+++ RunME.bat
@@ -1,15 +1,18 @@
 @echo on
 cd /d %~dp0
 
-REM Check if the virtual environment folder exists, if not create it and install required packages
+REM Check if the virtual environment folder exists, if not create it
 if not exist "SummarizePDF" (
     python -m venv SummarizePDF
-    call .\SummarizePDF\Scripts\activate.bat
-    call python -m pip install --upgrade pip
-    call python -m pip install -r requirements.txt
-    call .\SummarizePDF\Scripts\deactivate.bat
 )
 
+REM Activate the virtual environment and install required packages
+call .\SummarizePDF\Scripts\activate.bat
+call python -m pip install --upgrade pip
+call python -m pip install -r requirements.txt
+call .\SummarizePDF\Scripts\deactivate.bat
+
+REM Activate the virtual environment and run the script
 call .\SummarizePDF\Scripts\activate.bat
 python pdf_summarizer.py
 call .\SummarizePDF\Scripts\deactivate.bat
